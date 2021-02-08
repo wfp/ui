edc96e8d7df2cf66cf1e870d92dab5b3c6793e3a
Robert Gühne <robert@polyxostudios.de>
2021-02-08 01:27:52 +0100

    docs(Preview): multiple changes needed to the version bump of Storybook

--- .storybook/blocks/PurePreview.tsx
+++ .storybook/blocks/PurePreview.tsx
@@ -1,24 +1,13 @@
-import React, {
-  Children,
-  FunctionComponent,
-  ReactElement,
-  ReactNode,
-  useState,
-} from 'react';
-import { styled } from '@storybook/theming';
+import React, { Children, FunctionComponent, ReactElement, ReactNode, useState } from 'react';
 import { darken } from 'polished';
-import { logger } from '@storybook/client-logger';
+import { styled } from '@storybook/theming';
 
-import getBlockBackgroundStyle  from '../components/blocks/BlockBackgroundStyles'; //@storybook/components/dist/blocks/BlockBackgroundStyles';
-import { Source, SourceProps } from './Source';
-import {
-  ActionBar,
-  ActionItem,
-} from '@storybook/components' //'@storybook/components/dist/ActionBar/ActionBar';
-import { Toolbar } from '../components/blocks/Toolbar.tsx'//'@storybook/components/dist/blocks/Toolbar';
-
-import ReactDOMServer from 'react-dom/server';
-import pretty from 'pretty';
+import { getBlockBackgroundStyle } from  '../components/blocks/BlockBackgroundStyles';
+import { Source, SourceProps } from '@storybook/components';
+import { ActionBar, ActionItem } from '@storybook/components';
+import { Toolbar } from '../components/blocks/Toolbar.tsx';
+import { ZoomContext } from  '../components/blocks/ZoomContext';
+import { Zoom } from '../components/Zoom/Zoom';
 
 export interface PreviewProps {
   isColumn?: boolean;
@@ -27,22 +16,55 @@ export interface PreviewProps {
   isExpanded?: boolean;
   withToolbar?: boolean;
   className?: string;
+  additionalActions?: ActionItem[];
 }
 
-const ChildrenContainer = styled.div<PreviewProps>(({ isColumn, columns }) => ({
-  display: 'flex',
-  position: 'relative',
-  flexWrap: 'wrap',
-  padding: '10px 10px 30px 10px',
-  overflow: 'auto',
-  flexDirection: isColumn ? 'column' : 'row',
+type layout = 'padded' | 'fullscreen' | 'centered';
 
-  '> *': {
-    flex: columns ? `1 1 calc(100%/${columns} - 20px)` : `1 1 0%`,
-    marginTop: 20,
-    maxWidth: '100%',
-  },
-}));
+const ChildrenContainer = styled.div<PreviewProps & { layout: layout }>(
+  ({ isColumn, columns, layout }) => ({
+    display: isColumn || !columns ? 'block' : 'flex',
+    position: 'relative',
+    flexWrap: 'wrap',
+    overflow: 'auto',
+    flexDirection: isColumn ? 'column' : 'row',
+
+    '& .innerZoomElementWrapper > *': isColumn
+      ? {
+          width: layout !== 'fullscreen' ? 'calc(100% - 20px)' : '100%',
+          display: 'block',
+        }
+      : {
+          maxWidth: layout !== 'fullscreen' ? 'calc(100% - 20px)' : '100%',
+          display: 'inline-block',
+        },
+  }),
+  ({ layout = 'padded' }) =>
+    layout === 'centered' || layout === 'padded'
+      ? {
+          padding: '30px 20px',
+          margin: -10,
+          '& .innerZoomElementWrapper > *': {
+            width: 'auto',
+            border: '10px solid transparent!important',
+          },
+        }
+      : {},
+  ({ layout = 'padded' }) =>
+    layout === 'centered'
+      ? {
+          display: 'flex',
+          justifyContent: 'center',
+          justifyItems: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+        }
+      : {},
+  ({ columns }) =>
+    columns && columns > 1
+      ? { '.innerZoomElementWrapper > *': { minWidth: `calc(100% / ${columns} - 20px)` } }
+      : {}
+);
 
 const StyledSource = styled(Source)<{}>(({ theme }) => ({
   margin: 0,
@@ -53,15 +75,11 @@ const StyledSource = styled(Source)<{}>(({ theme }) => ({
   border: 'none',
 
   background:
-    theme.base === 'light'
-      ? 'rgba(0, 0, 0, 0.85)'
-      : darken(0.05, theme.background.content),
+    theme.base === 'light' ? 'rgba(0, 0, 0, 0.85)' : darken(0.05, theme.background.content),
   color: theme.color.lightest,
   button: {
     background:
-      theme.base === 'light'
-        ? 'rgba(0, 0, 0, 0.85)'
-        : darken(0.05, theme.background.content),
+      theme.base === 'light' ? 'rgba(0, 0, 0, 0.85)' : darken(0.05, theme.background.content),
   },
 }));
 
@@ -81,18 +99,12 @@ const PreviewContainer = styled.div<PreviewProps>(
 interface SourceItem {
   source?: ReactElement;
   actionItem: ActionItem;
-  actionItemHtml: ActionItem;
 }
 
 const getSource = (
   withSource: SourceProps,
   expanded: boolean,
-  setExpanded: Function,
-  htmlComponent: string,
-  name: string,
-  storyFn: any,
-  subcomponents: any,
-  parameters: any
+  setExpanded: Function
 ): SourceItem => {
   switch (true) {
     case !!(withSource && withSource.error): {
@@ -103,100 +115,18 @@ const getSource = (
           disabled: true,
           onClick: () => setExpanded(false),
         },
-        actionItemHtml: {
-          title: 'No html available',
-          disabled: true,
-          onClick: () => setExpanded(false),
-        },
       };
     }
-
-    case expanded === 'twig': {
-      const html = parameters.twig ? parameters.twig : 'not found';
-      const htmlSource = {
-        code: pretty(html),
-        dark: false,
-        language: 'jsx',
-      };
-      return {
-        source: <StyledSource {...htmlSource} dark />,
-        actionItem: { title: 'react', onClick: () => setExpanded(true) },
-        actionItemtwig: {
-          title: 'twig',
-          onClick: () => setExpanded(false),
-        },
-        actionItemHtml: {
-          title: 'html',
-          onClick: () => setExpanded(false),
-        },
-      };
-    }
-
-    case expanded === 'html': {
-      const html = ReactDOMServer.renderToStaticMarkup(storyFn).replace(
-        /role="[^"]*"/g,
-        ''
-      );
-      const htmlSource = {
-        code: pretty(html),
-        dark: false,
-        language: 'jsx',
-      };
-      return {
-        source: <StyledSource {...htmlSource} dark />,
-        actionItem: { title: 'react', onClick: () => setExpanded(true) },
-        actionItemtwig: {
-          title: 'twig',
-          onClick: () => setExpanded('twig'),
-        },
-        actionItemHtml: {
-          title: 'html',
-          onClick: () => setExpanded(false),
-        },
-      };
-    }
-
     case expanded: {
-      let a = '';
-
-      if (subcomponents)
-        Object.keys(subcomponents).forEach((element) => {
-          a = `${a}, ${element}`;
-        });
-      const reactSource = {
-        code: parameters.docs.source
-          ? withSource.code
-          : `import { ${name} ${a} } from "@wfp/ui";
-        
-${withSource.code}`,
-        dark: false,
-        language: 'jsx',
-      };
       return {
-        source: <StyledSource {...reactSource} dark />,
-        actionItem: { title: 'react', onClick: () => setExpanded(false) },
-        actionItemtwig: {
-          title: 'twig',
-          onClick: () => setExpanded('twig'),
-        },
-        actionItemHtml: {
-          title: 'html',
-          onClick: () => setExpanded('html'),
-        },
+        source: <StyledSource {...withSource} dark />,
+        actionItem: { title: 'Hide code', onClick: () => setExpanded(false) },
       };
     }
     default: {
       return {
         source: null,
-        actionItem: { title: 'react', onClick: () => setExpanded(true) },
-        actionItemtwig: {
-          title: 'twig',
-          onClick: () => setExpanded('twig'),
-        },
-        actionItemHtml: {
-          title: 'html',
-          onClick: () => setExpanded('html'),
-        },
+        actionItem: { title: 'Show code', onClick: () => setExpanded(true) },
       };
     }
   }
@@ -211,24 +141,6 @@ function getStoryId(children: ReactNode) {
   return null;
 }
 
-const Relative = styled.div({
-  position: 'relative',
-});
-
-const Scale = styled.div<{ scale: number }>(
-  {
-    position: 'relative',
-  },
-  ({ scale }) =>
-    scale
-      ? {
-          transform: scale !== 1 ? `scale(${1 / scale})` : undefined,
-          transformOrigin: 'top left',
-        }
-      : {}
-);
-
-console.log("styled",Toolbar);
 const PositionedToolbar = styled(Toolbar)({
   position: 'absolute',
   top: 0,
@@ -236,6 +148,23 @@ const PositionedToolbar = styled(Toolbar)({
   right: 0,
   height: 40,
 });
+
+const Relative = styled.div({
+  overflow: 'hidden',
+  position: 'relative',
+});
+
+const getLayout = (children: ReactElement[]): layout => {
+  return children.reduce((result, c) => {
+    if (result) {
+      return result;
+    }
+    if (typeof c === 'string' || typeof c === 'number') {
+      return 'padded';
+    }
+    return (c.props && c.props.parameters && c.props.parameters.layout) || 'padded';
+  }, undefined);
+};
 
 /**
  * A preview component for showing one or more component `Story`
@@ -247,48 +176,32 @@ const Preview: FunctionComponent<PreviewProps> = ({
   columns,
   children,
   withSource,
-  htmlComponent,
-  storyFn,
-  name,
   withToolbar = false,
   isExpanded = false,
+  additionalActions,
   className,
-  storyComponent,
-  subcomponents,
-  parameters,
   ...props
 }) => {
   const [expanded, setExpanded] = useState(isExpanded);
-  const { source, actionItem, actionItemHtml, actionItemtwig } = getSource(
-    withSource,
-    expanded,
-    setExpanded,
-    htmlComponent,
-    name,
-    storyComponent(),
-    subcomponents,
-    parameters
-  );
+  const { source, actionItem } = getSource(withSource, expanded, setExpanded);
   const [scale, setScale] = useState(1);
-  const previewClasses = className
-    ? `${className} sbdocs sbdocs-preview`
-    : 'sbdocs sbdocs-preview';
+  const previewClasses = [className].concat(['sbdocs', 'sbdocs-preview']);
 
-  if (withToolbar && Array.isArray(children)) {
-    logger.warn('Cannot use toolbar with multiple preview children, disabling');
-  }
-  const showToolbar = withToolbar && !Array.isArray(children);
+  const defaultActionItems = withSource ? [actionItem] : [];
+  const actionItems = additionalActions
+    ? [...defaultActionItems, ...additionalActions]
+    : defaultActionItems;
 
-  var actionItems = [];
-  if (parameters.code !== false) actionItems.push(actionItem);
-  if (parameters.twig) actionItems.push(actionItemtwig);
-  if (parameters.html !== false) actionItems.push(actionItemHtml);
+  // @ts-ignore
+  const layout = getLayout(Children.count(children) === 1 ? [children] : children);
+
   return (
     <PreviewContainer
-      {...{ withSource, withToolbar: showToolbar }}
+      {...{ withSource, withToolbar }}
       {...props}
-      className={previewClasses}>
-      {showToolbar && (
+      className={previewClasses.join(' ')}
+    >
+      {withToolbar && (
         <PositionedToolbar
           border
           zoom={(z) => setScale(scale * z)}
@@ -297,17 +210,25 @@ const Preview: FunctionComponent<PreviewProps> = ({
           baseUrl="./iframe.html"
         />
       )}
-      <Relative>
-        <ChildrenContainer isColumn={isColumn} columns={columns}>
-          {Array.isArray(children) ? (
-            children.map((child, i) => <div key={i.toString()}>{child}</div>)
-          ) : (
-            <Scale scale={scale}>{children}</Scale>
-          )}
-        </ChildrenContainer>
-
-        {withSource && <ActionBar actionItems={actionItems} />}
-      </Relative>
+      <ZoomContext.Provider value={{ scale }}>
+        <Relative className="docs-story">
+          <ChildrenContainer
+            isColumn={isColumn || !Array.isArray(children)}
+            columns={columns}
+            layout={layout}
+          >
+            <Zoom.Element scale={scale}>
+              {Array.isArray(children) ? (
+                // eslint-disable-next-line react/no-array-index-key
+                children.map((child, i) => <div key={i}>{child}</div>)
+              ) : (
+                <div>{children}</div>
+              )}
+            </Zoom.Element>
+          </ChildrenContainer>
+          <ActionBar actionItems={actionItems} />
+        </Relative>
+      </ZoomContext.Provider>
       {withSource && source}
     </PreviewContainer>
   );
